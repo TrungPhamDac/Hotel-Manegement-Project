04509abf9862d01f6b241de270da428f31a25d78
TrungPhamDac <19522434@gm.uit.edu.vn>
2021-05-25 22:02:24 +0700

    Add project

---
+++ src/Model/Phong.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author asus
+ */
+public class Phong {
+    private String MaPhg;
+    private String MaLoaiPhg;
+    private String Mota;
+    private int DonGia;
+    private String DonVi;
+
+    public String getMaPhg() {
+        return MaPhg;
+    }
+
+    public void setMaPhg(String MaPhg) {
+        this.MaPhg = MaPhg;
+    }
+
+    public String getMaLoaiPhg() {
+        return MaLoaiPhg;
+    }
+
+    public void setMaLoaiPhg(String MaLoaiPhg) {
+        this.MaLoaiPhg = MaLoaiPhg;
+    }
+
+    public String getMota() {
+        return Mota;
+    }
+
+    public void setMota(String Mota) {
+        this.Mota = Mota;
+    }
+
+    public int getDonGia() {
+        return DonGia;
+    }
+
+    public void setDonGia(int DonGia) {
+        this.DonGia = DonGia;
+    }
+
+    public String getDonVi() {
+        return DonVi;
+    }
+
+    public void setDonVi(String DonVi) {
+        this.DonVi = DonVi;
+    }
+    
+    
+}

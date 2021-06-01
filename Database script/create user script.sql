0fae93a332546de7d602d41cc00b9cd44c552dbd
Noi-Nguyen <ngthnoi00@gmail.com>
2021-06-01 13:23:13 +0700

    adding password to db user

--- Database script/create user script.sql
+++ Database script/create user script.sql
@@ -1,2 +1,2 @@
-create user hotelmng;
+create user hotelmng identified by 123456;
 grant create session, create table, create view, create any trigger, create any procedure, create sequence, create synonym, create any type, unlimited tablespace to hotelmng;

7feede3cc0e13a43146eea7a50dbe4a123a41d32
TrungPhamDac <19522434@gm.uit.edu.vn>
2021-06-13 00:29:54 +0700

    No change

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -12,6 +12,6 @@ import java.util.GregorianCalendar;
 public class Main {
     public static void main(String[] args){
         new MainJFrame().setVisible(true);
-//    new LoginForm().setVisible(true);
+//        new LoginForm().setVisible(true);
     }
 }

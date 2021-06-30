83b2ce5f701c9823cf2c351c97271bba7fdd4b85
Le` Nooiz <52886036+Noi-Nguyen@users.noreply.github.com>
2021-06-30 09:10:47 +0700

    add look and feel

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -3,6 +3,7 @@ package Main;
 import View.AdminJFrame;
 import View.LoginForm;
 import View.MainJFrame;
+import com.formdev.flatlaf.FlatLightLaf;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 
@@ -13,15 +14,15 @@ import javax.swing.UIManager;
 public class Main {
     public static void main(String[] args){
         try {
-            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(LoginForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         
 //        new AdminJFrame().setVisible(true);
-        new MainJFrame().setVisible(true);
-//        LoginForm lgform = new LoginForm();
-//        lgform.setVisible(true);
+//        new MainJFrame().setVisible(true);
+        LoginForm lgform = new LoginForm();
+        lgform.setVisible(true);
 
     }
 }
